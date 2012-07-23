ac24e2e0b4dcceac9990581f4bac940c32280f0a
Kevin Herrera <me@kevingh.com>
2012-07-23 01:18:51 -0700

    Adding current work.

---
+++ src/tests/load.php
@@ -0,0 +1,11 @@
+<?php
+
+    spl_autoload_register(function($class)
+    {
+        $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')) . '.php';
+
+        if (file_exists($path))
+        {
+            require $path;
+        }
+    });
