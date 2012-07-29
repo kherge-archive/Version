2b1138bab1c31eb0693a4515d7ccfdf8a80bbbb9
Kevin Herrera <me@kevingh.com>
2012-07-28 23:48:40 -0700

    Using coding styles defined by PSR-1 and PSR-2.

--- src/tests/load.php
+++ src/tests/load.php
@@ -4,8 +4,7 @@
     {
         $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')) . '.php';
 
-        if (file_exists($path))
-        {
+        if (file_exists($path)) {
             require $path;
         }
     });
